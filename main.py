9cc1fd153b849969cae86770c25fd70e991358ef
Laotze de Abreu <108775728+laotzee@users.noreply.github.com>
2025-06-04 11:46:46 -0400

    Update main.py
    The comments hinting the types of variables and output of functions were replaced with the built in type hint of Python. Optional from typing was imported to contribute to such purpose.
    Besides, a progress bar was added to the timer which is update along the timer.

--- main.py
+++ main.py
@@ -1,35 +1,67 @@
 from time import sleep
+from typing import Optional
 import os
 
-LONG_P = ("10m", "50m")
+LONG_P: tuple = ("10m", "50m")
+SHORT_P: tuple = ("5m", "25m")
+CURRENT_P: tuple = SHORT_P
 
-SHORT_P = ("5m", "25m")
+FINAL_MESSAGE: str = "Time's up"
 
-CURRENT_P = SHORT_P
+ZERO = "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+TEN = "██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+TWENTY = "████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+THIRTY = "██████▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+FORTY = "████████▒▒▒▒▒▒▒▒▒▒▒▒"
+FIFTY = "██████████▒▒▒▒▒▒▒▒▒▒"
+SIXTY = "████████████▒▒▒▒▒▒▒▒"
+SEVENTY = "██████████████▒▒▒▒▒▒"
+EIGHTY = "████████████████▒▒▒▒"
+NINETY = "██████████████████▒▒"
+HUNDRED = "████████████████████"
+BAR = [ZERO, TEN, TWENTY, THIRTY, FORTY, FIFTY, SIXTY, SEVENTY, EIGHTY, NINETY,
+    HUNDRED]
+#BAR.reverse()
 
 def clear_screen():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
+def get_percentage(total_time: int, current_time: int) -> int:
+    """Calculates the total progress of the timer in percentage and returns
+    only the first number"""
 
-# string -> int
-def get_seconds(user_input):
+    percentage = str((100 * current_time) // total_time)
+    if len(percentage) == 3:
+        return int(percentage[:2])
+    else:
+        return int(percentage[:1])
+
+assert get_percentage(100, 100) == 10
+assert get_percentage(100, 50) == 5
+assert get_percentage(100, 25) == 2
+assert get_percentage(100, 99) == 9
+
+def get_seconds(user_input: str) -> Optional[int]:
     """Manages user input to get total amount of seconds"""
     if user_input.isdigit():
         return int(user_input)
     else:
         user_input = user_input.lower()
+
         if user_input[-1] == 's': #seconds
             return int(user_input[:-1])
         elif user_input[-1] == 'm': #minutes
             return 60*(int(user_input[:-1]))
         elif user_input[-1] == 'h': #hours
             return 3600*(int(user_input[:-1]))
+
         elif user_input == 'p': # P stands for pomodoro
             return get_seconds(CURRENT_P[1])
         elif user_input == 'b': # B stands for break
             return get_seconds(CURRENT_P[0])
+
         else:
             print('No proper input given, please try again\n')
             exit()
@@ -39,8 +71,7 @@ assert get_seconds('3m') == 180
 assert get_seconds('60s') == 60
 assert get_seconds('5') == 5
 
-# int -> string
-def format_time(h, m, s):
+def format_time(h: int, m: int, s: int, bar: str) -> str:
     """Formats time to be properly displayed"""
 
     t = f'{h:02}:{m:02}:{s:02}'
@@ -58,22 +89,25 @@ def format_time(h, m, s):
 ⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿
 ⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣶⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"""
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+     {bar}"""
+
 
     return tomato
 
-# string -> None
-def show_time(t):
+def show_time(s: int, m: int, h: int, bar: tuple) -> None:
     """Takes formatted time to display it to the screen and adds a second of
     delay"""
-    clear_screen()
-    print(t, end='\r')
-    sleep(1)
 
-# int, string -> int
-def update_val(t, suffix='s'):
-    """time is int, and suffix is either 's', 'm' or 'h'.
-    It will calculate the amount of time in terms of the giving suffix"""
+    for i in range(2):
+        formatted_timer = format_time(h, m, s, bar[i])
+        clear_screen()
+        print(formatted_timer, end="\r")
+        sleep(0.5)
+
+def update_val(t: int, suffix: str) -> int:
+    """It will calculate the amount of time in terms of the giving suffix"""
     suffix = suffix.lower()
     if suffix == 's':
         return t%60
@@ -85,19 +119,20 @@ def update_val(t, suffix='s'):
 assert update_val(70, 's') == 10
 assert update_val(360, 'm') == 6
 assert update_val(3789, 'h') == 1
-assert update_val(325) == 25
-
 
 raw_time = input('Enter the time along with a suffix (h/m/s):\n')
 timer_seconds = get_seconds(raw_time)
-message = "Time's up"
+total_time = timer_seconds
 
 while timer_seconds >= 0:
-    seconds = update_val(timer_seconds)
+    seconds = update_val(timer_seconds, 's')
     minutes = update_val(timer_seconds, 'm')
     hours = update_val(timer_seconds, 'h')
-    timer = format_time(hours, minutes, seconds)
-    show_time(timer)
+
+    percentage = get_percentage(total_time, timer_seconds)
+    current_bars = (BAR[percentage], BAR[percentage - 1])
+
+    show_time(seconds, minutes, hours, current_bars)
     timer_seconds -= 1
 
-print(message)
+print(FINAL_MESSAGE)
