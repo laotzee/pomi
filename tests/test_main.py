d9a478cddacf23c9aa636dfe6c36e235edaca821
laotzee <laotzeabreu@proton.me>
2025-08-04 21:36:03 -0400

    Create  tests for get_percentage and get_seconds functions

---
+++ tests/test_main.py
@@ -0,0 +1,48 @@
+import pytest
+from lvmdbusd.cmdhandler import total_time
+
+import src.main as pomi
+
+# TODO
+# 1.- Add test for invalid input
+# 2.- Alter get_seconds function to work properly
+# 3.- Add regex to get_seconds to discard bad user input
+# 2.-
+# 2.-
+# 2.-
+# 2.-
+
+get_seconds_input = [
+    ('1h', 3600),
+    ('3m', 180),
+    ('60s', 60),
+    ('5', 5),
+    ('p', pomi.CURRENT_P[1]),
+    ('b', pomi.CURRENT_P[0]),
+    ('j4s', None),            # matches suffix but not the rest
+    ('lkdfs##$sd234', None),  # has symbols
+    ('sdff33', None),         # alphanumeric but not properly formatted
+    ('', None),               # empty input
+]
+
+get_percentage_input = [
+    (100, 100, 10),
+    (100, 50, 5),
+    (100, 25, 2),
+    (100, 99, 9),
+]
+
+
+@pytest.mark.parametrize('user_input, expected_output', get_seconds_input)
+def test_get_seconds(user_input, expected_output):
+
+    pomi.get_seconds(user_input)
+
+def test_update_val():
+    pass
+
+@pytest.mark.parametrize('total_time, current_time, expected_output', get_percentage_input)
+def test_get_percentage(total_time, current_time, expected_output):
+
+    assert pomi.get_percentage(total_time=total_time, current_time=current_time) ==  expected_output
+
