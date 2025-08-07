7b5a927dd2a2d28aa540a83913ab640e954fdd4d
laotzee <laotzeabreu@proton.me>
2025-08-06 20:44:42 -0400

    Add test for str_to_seconds function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,6 @@
 import pytest
 import src.main as pomi
 
-# TODO
-# 1.- Add test for invalid input
-# 2.- Alter get_seconds function to work properly
-# 3.- Add regex to get_seconds to discard bad user input
-# 2.-
-# 2.-
-# 2.-
-# 2.-
-
 get_seconds_input = [
     ('1h', 3600),
     ('3m', 180),
@@ -22,6 +13,7 @@ get_seconds_input = [
     ('sdff33', None),         # alphanumeric but not properly formatted
     ('', None),               # empty input
 ]
+
 get_percentage_input = [
     (100, 100, 10),
     (100, 50, 5),
@@ -32,6 +24,12 @@ update_val_input = [
     (70, 's', 10),
     (360, 'm', 6),
     (3789, 'h', 1),
+]
+
+str_to_secons_input = [
+    (1, '200', 200),
+    (60, '5', 300),
+    (3600, '2', 7200),
 ]
 
 
@@ -49,6 +47,11 @@ def test_get_percentage(total_time, current_time, expected_output):
     pomi.get_percentage(total_time, current_time)
 
 @pytest.mark.parametrize('t, suffix, expected_output', update_val_input)
-def  test_update_val(t, suffix, expected_output):
+def test_update_val(t, suffix, expected_output):
 
     pomi.update_val(t, suffix)
+
+@pytest.mark.parametrize('multiplier, time, expected_output', str_to_secons_input)
+def test_str_to_seconds(multiplier, time, expected_output):
+
+    pomi.str_to_seconds(multiplier, time)
